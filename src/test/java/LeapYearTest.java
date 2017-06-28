65ab102e342c75994605feebfbe621a2aad7d5a2
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-06-28 13:29:45 -0700

    add first junit test code

---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LeapYearTest{
+
+  @Test
+  public void isLeapYear_forNumberDivisibleByFour_true(){
+    
+  }
+
+
+
+
+
+
+
+}
